44c0248f534624af890bd27e9de8868dbf5457ad
DevGlee <56635346+DevGlee@users.noreply.github.com>
2019-12-27 15:56:18 -0500

    edit

--- semaine.c
+++ semaine.c
@@ -3,37 +3,51 @@
 int main (void)
 {
     int jour;
+    
+    /*  LUNDI ==2
+        MARDI ==3
+        MERCREDI ==4
+        JEUDI ==5
+        VENDREDI ==6
+        SAMEDI ==0
+        DIMANCHE ==1*/
 
     printf("Donner un jour \n");
-    scanf("%d", jour);
+    scanf("%d", &jour);
 
-    printf("Le reste est %d", jour%jour-1);
-    
+        jour=(jour-1)%7;
+        printf("Le reste est %d \n", jour);
+
     switch (jour)
     {
-    case :
-    break;
-
-    case :
-    break;
-
-    case :
-    break;
-
-    case :
-    break;
-
-    case :
-    break;
-
-    case :
-    break;
-
-    case :
-    break;
-    
+        case 0:
+        printf("Nous sommes samedi ! \n");
+        break;
+        case 1:
+        printf("Nous sommes dimanche ! \n");
+        break;
+        case 2:
+        printf("Nous sommes lundi ! \n");
+        break;
+        case 3:
+        printf("Nous sommes mardi ! \n");
+        break;
+        case 4:
+        printf("Nous sommes mercredi ! \n");
+        break;
+        case 5:
+        printf("Nous sommes jeudi ! \n");
+        break;
+        case 6:
+        printf("Nous sommes vendredi ! \n");
+        break;
     
     default:
         break;
     }
+    
+    
+    
+    return 0;
+    
 }
